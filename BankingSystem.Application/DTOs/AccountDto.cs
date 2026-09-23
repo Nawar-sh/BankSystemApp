16db56da10968ec8f0818f2f83e68fed79576cf2
Nawar-sh <shalashnawar@gmail.com>
2026-09-23 22:22:21 +0300

    Feat: Add IAccountRepository interface and Account DTOs in Application layer

---
+++ BankingSystem.Application/DTOs/AccountDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankingSystem.Application.DTOs
+{
+    public class AccountDto
+    {
+        public int Id { get; set; }
+        public string AccountNumber { get; set; } = string.Empty;
+        public string AccountType { get; set; } = string.Empty;
+        public decimal Balance { get; set; }
+        public bool IsActive { get; set; }
+    }
+
+    public class TransactionRequestDto
+    {
+        public int AccountId { get; set; }
+        public decimal Amount { get; set; }
+        public string Details { get; set; } = string.Empty;
+    }
+}
---
+++ BankingSystem.Application/Interfaces/IAccountRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using BankingSystem.Domain.Entities;
+
+namespace BankingSystem.Application.Interfaces
+{
+    public interface IAccountRepository
+    {
+        Task<Account?> GetByIdAsync(int id);
+        Task<IEnumerable<Account>> GetByCustomerIdAsync(int customerId);
+        Task CreateAccountAsync(Account account);
+        Task<bool> DepositAsync(int accountId, decimal amount, string details);
+        Task<bool> WithdrawAsync(int accountId, decimal amount, string details);
+        Task<bool> SaveChangesAsync();
+    }
+}
